16d492a61be9745f2c91a5937c20a9cdc45a92c1
Ben Gyori <ben.gyori@gmail.com>
2018-02-06 13:16:27 -0500

    Simplify biopax fixing script

--- run_assembly/fix_biopax.py
+++ run_assembly/fix_biopax.py
@@ -4,9 +4,8 @@ chemicals in the conversion can sometimes have lists in them.
 This script flattens these lists."""
 
 from util import pklload, pkldump
-from indra.sources.biopax.processor import _list_listify
-import indra.tools.assemble_corpus as ac
 from indra.statements import Conversion
+from indra.sources.biopax.processor import _list_listify
 
 def merge(list_of_lists):
     ll = []
@@ -15,8 +14,7 @@ def merge(list_of_lists):
     return ll
 
 # Load the pickle from the data directory
-stmts = ac.load_statements('data/bioexp_biopax.pkl')
-new_stmts = []
+stmts = pklload('biopax')
 for stmt in stmts:
     if isinstance(stmt, Conversion):
         if any([isinstance(l, list) for l in stmt.obj_from]):
@@ -25,4 +23,4 @@ for stmt in stmts:
         if any([isinstance(l, list) for l in stmt.obj_from]):
             print(stmt)
         stmt.obj_to = merge(_list_listify(stmt.obj_to))
-stmts = pkldump(stmts, 'biopax_fixed')
+pkldump(stmts, 'biopax_fixed')
