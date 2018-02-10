1ba90dea7dcc313b80d614a76fd03f5626a52519
John Bachman <bachmanjohn@gmail.com>
2018-02-10 11:55:57 -0500

    More general assembly Makefile

--- run_assembly/assemble_sources.py
+++ run_assembly/assemble_sources.py
@@ -8,6 +8,15 @@ from indra.mechlinker import MechLinker
 if __name__ == '__main__':
     #sources = ['bel', 'biopax', 'reach', 'sparser']
     cmd = sys.argv[1]
+    assembly_cmds = (
+            'filter_no_hypothesis', 'map_grounding', 'filter_grounded_only',
+            'filter_genes_only', 'filter_human_only', 'expand_families',
+            'filter_gene_list', 'map_sequence', 'preassembled',
+            'filter_belief', 'filter_top_level', 'filter_enzyme_kinase',
+            'filter_mod_nokinase', 'reduce_activities', 'reduce_mods')
+    if cmd in assembly_cmds:
+        input_file = sys.argv[2]
+        output_file = sys.argv[3]
     # Break each assembly cmd down into intermediate steps
     if cmd == 'signor':
         sp = signor.SignorProcessor()
@@ -24,61 +33,61 @@ if __name__ == '__main__':
             stmts += ac.load_statements(source)
         pkldump(stmts, 'all_raw')
     elif cmd == 'filter_no_hypothesis':
-        stmts = pklload('all_raw')
-        stmts = ac.filter_no_hypothesis(stmts, save=prefixed_pkl(cmd))
+        stmts = pklload(input_file)
+        stmts = ac.filter_no_hypothesis(stmts, save=prefixed_pkl(output_file))
     elif cmd == 'map_grounding':
-        stmts = pklload('filter_no_hypothesis')
-        stmts = ac.map_grounding(stmts, save=prefixed_pkl(cmd))
+        stmts = pklload(input_file)
+        stmts = ac.map_grounding(stmts, save=prefixed_pkl(output_file))
     elif cmd == 'filter_grounded_only':
-        stmts = pklload('map_grounding')
-        stmts = ac.filter_grounded_only(stmts, save=prefixed_pkl(cmd))
+        stmts = pklload(input_file)
+        stmts = ac.filter_grounded_only(stmts, save=prefixed_pkl(output_file))
     elif cmd == 'filter_genes_only':
-        stmts = pklload('filter_grounded_only')
+        stmts = pklload(input_file)
         stmts = ac.filter_genes_only(stmts, specific_only=False,
-                                     save=prefixed_pkl(cmd))
+                                     save=prefixed_pkl(output_file))
     elif cmd == 'filter_human_only':
-        stmts = pklload('filter_genes_only')
-        stmts = ac.filter_human_only(stmts, save=prefixed_pkl(cmd))
+        stmts = pklload(input_file)
+        stmts = ac.filter_human_only(stmts, save=prefixed_pkl(output_file))
     elif cmd == 'expand_families':
-        stmts = pklload('filter_human_only')
-        stmts = ac.expand_families(stmts, save=prefixed_pkl(cmd))
+        stmts = pklload(input_file)
+        stmts = ac.expand_families(stmts, save=prefixed_pkl(output_file))
     elif cmd == 'filter_gene_list':
-        stmts = pklload('expand_families')
+        stmts = pklload(input_file)
         # Load the genes from the outer build directory
         with open('../build/prior_genes.txt', 'rt') as f:
             data_genes = [line.strip() for line in f.readlines()]
         stmts = ac.filter_gene_list(stmts, data_genes, 'one',
-                                    save=prefixed_pkl(cmd))
+                                    save=prefixed_pkl(output_file))
     elif cmd == 'map_sequence':
-        stmts = pklload('filter_gene_list')
-        stmts = ac.map_sequence(stmts, save=prefixed_pkl(cmd))
+        stmts = pklload(input_file)
+        stmts = ac.map_sequence(stmts, save=prefixed_pkl(output_file))
     elif cmd == 'preassembled':
-        stmts = pklload('map_sequence')
+        stmts = pklload(input_file)
         stmts = ac.run_preassembly(stmts, return_toplevel=False,
-                                   save=prefixed_pkl(cmd),
+                                   save=prefixed_pkl(output_file),
                                    poolsize=16)
     elif cmd == 'filter_belief':
-        stmts = pklload('preassembled')
-        stmts = ac.filter_belief(stmts, 0.95, save=prefixed_pkl(cmd))
+        stmts = pklload(input_file)
+        stmts = ac.filter_belief(stmts, 0.95, save=prefixed_pkl(output_file))
     elif cmd == 'filter_top_level':
-        stmts = pklload('filter_belief')
-        stmts = ac.filter_top_level(stmts, save=prefixed_pkl(cmd))
+        stmts = pklload(input_file)
+        stmts = ac.filter_top_level(stmts, save=prefixed_pkl(output_file))
     elif cmd == 'filter_enzyme_kinase':
-        stmts = pklload('filter_top_level')
-        stmts = ac.filter_enzyme_kinase(stmts, save=prefixed_pkl(cmd))
+        stmts = pklload(input_file)
+        stmts = ac.filter_enzyme_kinase(stmts, save=prefixed_pkl(output_file))
     elif cmd == 'filter_mod_nokinase':
-        stmts = pklload('filter_enzyme_kinase')
-        stmts = ac.filter_mod_nokinase(stmts, save=prefixed_pkl(cmd))
+        stmts = pklload(input_file)
+        stmts = ac.filter_mod_nokinase(stmts, save=prefixed_pkl(output_file))
     elif cmd == 'reduce_activities':
-        stmts = pklload('filter_mod_nokinase')
+        stmts = pklload(input_file)
         ml = MechLinker(stmts)
         ml.gather_explicit_activities()
         ml.reduce_activities()
-        pkldump(ml.statements, cmd)
+        pkldump(ml.statements, output_file)
     elif cmd == 'reduce_mods':
-        stmts = pklload('reduce_activities')
+        stmts = pklload(input_file)
         ml = MechLinker(stmts)
         ml.gather_modifications()
         ml.reduce_modifications()
-        pkldump(ml.statements, cmd)
+        pkldump(ml.statements, output_file)
 
