b11d73587244889d8721414454bbf1ad2a099f42
Ben Gyori <ben.gyori@gmail.com>
2019-05-28 19:01:02 -0400

    Refactor transfer_s3 to make it importable

--- bioexp/transfer_s3.py
+++ bioexp/transfer_s3.py
@@ -3,35 +3,44 @@ import shutil
 from os.path import dirname, join, basename
 import boto3
 
-data_dir = join(dirname(__file__), '..', 'data')
-
-action = sys.argv[1]
-if action not in ('get', 'put'):
-    usage =  "Usage:\n"
-    usage += "  %s get <key_name> <target_dir>\n" % sys.argv[0]
-    usage += "  %s put <filename>" % sys.argv[0]
-    print(usage)
-    sys.exit(1)
 
 bucket_name = 'bioexp-paper'
 
-s3_client = boto3.client('s3')
 
-if action == 'get':
-    s3_key = basename(sys.argv[2])
-    target_dir = sys.argv[3]
-    filename = join(target_dir, s3_key)
+def download_from_s3(key, filename):
     # Get data from S3
-    print("Getting %s..." % s3_key)
-    obj_response = s3_client.get_object(Bucket=bucket_name, Key=s3_key)
+    print("Getting %s..." % key)
+    obj_response = s3_client.get_object(Bucket=bucket_name, Key=key)
     data = obj_response['Body']
     # Write data to a file
     with open(filename, 'wb') as f:
         print("Downloading to %s..." % filename)
         shutil.copyfileobj(data, f)
-else:
-    filename = sys.argv[2]
-    s3_key = basename(sys.argv[2])
+
+
+def upload_to_s3(key, filename):
     with open(filename, 'rb') as f:
         s3_client.put_object(Bucket=bucket_name, Key=s3_key, Body=f)
 
+
+if __name__ == '__main__':
+    action = sys.argv[1]
+    if action not in ('get', 'put'):
+        usage =  "Usage:\n"
+        usage += "  %s get <key_name> <target_dir>\n" % sys.argv[0]
+        usage += "  %s put <filename>" % sys.argv[0]
+        print(usage)
+        sys.exit(1)
+
+
+    s3_client = boto3.client('s3')
+
+    if action == 'get':
+        s3_key = basename(sys.argv[2])
+        target_dir = sys.argv[3]
+        filename = join(target_dir, s3_key)
+        download_from_s3(s3_key, filename)
+    else:
+        filename = sys.argv[2]
+        s3_key = basename(sys.argv[2])
+        upload_to_s3(s3_key, filename)
