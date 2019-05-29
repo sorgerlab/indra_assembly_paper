fd7ddb7d2022e60a1dc114d7c19aa235be9fa6ad
Ben Gyori <ben.gyori@gmail.com>
2019-05-29 17:26:40 -0400

    Add function to process TRIPS EKBs

--- sources/process_sources.py
+++ sources/process_sources.py
@@ -130,7 +130,6 @@ def _process_medscan_get_stmts(fname):
 
 
 def process_medscan(data_folder):
-    import glob
     from multiprocessing import Pool
     from indra.sources import medscan
     # Process PMID file first
@@ -139,7 +138,7 @@ def process_medscan(data_folder):
     pmid_stmts = mp.statements
     # Process PMC files next
     file_pattern = os.path.join(data_folder, 'medscan', 'pmids', '*.csxml')
-    fnames = glob.glbo(file_pattern)
+    fnames = glob.glob(file_pattern)
     # Now process files in parallel
     pool = Pool(4)
     stmts_ll = pool.map(_process_medscan_get_stmts, fnames)
@@ -178,6 +177,22 @@ def process_reach(data_folder):
     stmts = []
     for stmts_l in stmts_ll:
         stmts += stmts_l
+    return stmts
+
+
+def process_trips(data_folder):
+    from indra.sources import trips
+    file_pattern = os.path.join(data_folder, 'trips', '*.ekb')
+    fnames = glob.glob(file_pattern)
+    stmts = []
+    for fname in fnames:
+        pmid = os.path.basename(fname)[:-4]
+        tp = trips.process_xml_file(fname)
+        if tp:
+            for stmt in tp.statements:
+                for ev in stmt.evidence:
+                    ev.pmid = pmid
+                stmts.append(stmt)
     return stmts
 
 
