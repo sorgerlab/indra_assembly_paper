08cd830d65f0b08418dcb9cf5776d629d4366b08
Ben Gyori <ben.gyori@gmail.com>
2020-02-17 11:20:23 -0500

    Implement general uncertainty minimization

--- bioexp/curation/optimize_curation.py
+++ bioexp/curation/optimize_curation.py
@@ -13,33 +13,45 @@ def pred_uncertainty(fun, param_samples, x_values, x_probs=None):
     return sum_var
 
 
-def add_proposed_data(correct_by_num_ev, exp_by_num_ev):
+def add_proposed_data(correct_by_num_ev, exp_by_num_ev, p):
     proposed_correct_by_num_ev = deepcopy(correct_by_num_ev)
     for num_ev, nexp in exp_by_num_ev.items():
-        if num_ev in correct_by_num_ev:
-            p = np.mean(correct_by_num_ev[num_ev])
-        else:
-            p = 0.5
+        if num_ev not in correct_by_num_ev:
             proposed_correct_by_num_ev[num_ev] = []
-        nexpcorr = int(np.round(p * nexp))
+        pexp = belief(num_ev, *p)
+        nexpcorr = int(np.round(pexp * nexp))
         proposed_correct_by_num_ev[num_ev] += \
             (nexpcorr*[1] + (nexp-nexpcorr)*[0])
     return proposed_correct_by_num_ev
+
+
+def proposal_uncertainty(exp_by_num_ev):
+    proposed_correct_by_num_ev = add_proposed_data(correct_by_num_ev,
+                                                   exp_by_num_ev,
+                                                   (0.3, 0.3))
+    samples = get_posterior_samples(proposed_correct_by_num_ev)
+    pred_unc = pred_uncertainty(belief, samples, range(1, 11))
+    print('Predicted overall uncertainty with %s: %.2E' %
+          (str(exp_by_num_ev), pred_unc))
+    return pred_unc
+
+
+def optimize_proposal_uncertainty(cost=100):
+    # Both parameters have to be between 0 and 1
+    bounds = [(0, cost)] * 10
+    cost_constraint = {'type': 'ineq',
+                       'fun': lambda x: (sum(x * range(1, len(x)+1)) - cost)}
+    fun = lambda x: proposal_uncertainty({i+1: int(np.floor(x[i]))
+                                          for i in range(10)})
+    res = scipy.optimize.minimize(fun,
+                                  bounds=bounds,
+                                  constraints=[cost_constraint],
+                                  x0=([cost] + [0]*9))
+    return res.x
 
 
 if __name__ == '__main__':
     stmts = load_reach_curated_stmts()
     source_list = ('bioexp_paper_tsv', 'bioexp_paper_reach')
     correct_by_num_ev = preprocess_data(source_list, stmts)
-    proposed_data = [{1: 50, 10: 50},
-                     {10: 100},
-                     {1: 100},
-                     {1: 25, 3: 25, 6: 25, 10: 25}]
-    correct_by_num_ev = {}
-    for exp_by_num_ev in proposed_data:
-        proposed_correct_by_num_ev = add_proposed_data(correct_by_num_ev,
-                                                       exp_by_num_ev)
-        samples = get_posterior_samples(proposed_correct_by_num_ev)
-        pred_unc = pred_uncertainty(belief, samples, range(1, 11))
-        print('Predicted overall uncertainty with %s: %.2E' %
-              (str(exp_by_num_ev), pred_unc))
+    optimize_proposal_uncertainty()
