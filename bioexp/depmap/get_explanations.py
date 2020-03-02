252eaadf651177e01036112a1bc68f0af151c815
John Bachman <bachmanjohn@gmail.com>
2020-03-02 17:12:37 -0500

    Working script to match_correlations

--- bioexp/depmap/get_explanations.py
+++ bioexp/depmap/get_explanations.py
@@ -7,6 +7,12 @@ corr_file = "/Users/johnbachman/Dropbox/1johndata/knowledge file/Biology/" \
             "Research/Big Mechanism/depmap_analysis/notebooks/data/19q4/" \
             "dep_z.h5"
 
+print(f"Loading {corr_file}")
 corr = pd.read_hdf(corr_file)
 inet = pklload('signor_indranet')
+inet_dg = inet.to_digraph()
+print("Sampling")
+samp = corr.sample(1000, axis=0).sample(1000, axis=1)
+print(f"match_correlations")
+res = match_correlations(samp, inet_dg)
 
