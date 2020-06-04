ca12e00f23300f549087a7d562628498c4e49c6e
John Bachman <bachmanjohn@gmail.com>
2020-06-04 15:01:41 -0400

    Make corr file a cmd line arg

--- bioexp/depmap/get_explanations.py
+++ bioexp/depmap/get_explanations.py
@@ -3,9 +3,10 @@ from depmap_analysis.scripts.depmap_script2 import match_correlations
 from bioexp.util import pklload
 
 
-corr_file = "/Users/johnbachman/Dropbox/1johndata/knowledge file/Biology/" \
-            "Research/Big Mechanism/depmap_analysis/notebooks/data/19q4/" \
-            "dep_z.h5"
+# "/Users/johnbachman/Dropbox/1johndata/knowledge file/Biology/" \
+# "Research/Big Mechanism/depmap_analysis/notebooks/data/19q4/" \
+# "dep_z.h5"
+corr_file = sys.argv[1]
 
 print(f"Loading {corr_file}")
 corr = pd.read_hdf(corr_file)
