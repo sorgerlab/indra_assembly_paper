3720ba16cb13298790e170d2cc84152212ab54fb
John Bachman <bachmanjohn@gmail.com>
2020-06-09 12:20:02 -0400

    Make get_ev_distro Makefile compatible

--- bioexp/curation/get_ev_distro.py
+++ bioexp/curation/get_ev_distro.py
@@ -1,13 +1,18 @@
 import sys
+from os.path import abspath, dirname, join
 import json
 import pickle
 from collections import Counter
-
+from bioexp.util import prefixed_file
 
 reader = sys.argv[1]
 
 
-with open('../../data/bioexp_asmb_preassembled.pkl', 'rb') as fh:
+stmts_file = join(dirname(abspath(__file__)), '..', '..', 'data',
+                  'bioexp_asmb_preassembled.pkl')
+
+
+with open(stmts_file, 'rb') as fh:
     stmts = pickle.load(fh)
 
 
@@ -20,17 +25,21 @@ for stmt in stmts:
         pmid_cnt.append(npmids) 
         ev_cnt.append(len(reader_ev))
 
+
 pmid_distro = [Counter(pmid_cnt)[i] for i in range(1,11)]
 ev_distro = [Counter(ev_cnt)[i] for i in range(1,11)]
 
 
-with open(f'{reader}_stmt_pmid_distribution.json', 'w') as fh:
+pmid_file = prefixed_file(f'{reader}_stmt_pmid_distribution', '.json')
+with open(pmid_file, 'w') as fh:
     dd = {(i+1): pmid_distro[i] for i in range(10)}
     s = sum(dd.values())
     dd = {k: v/s for k, v in dd.items()}
     json.dump(dd, fh, indent=1)
 
-with open(f'{reader}_stmt_evidence_distribution.json', 'w') as fh:
+
+ev_file = prefixed_file(f'{reader}_stmt_evidence_distribution', '.json')
+with open(ev_file, 'w') as fh:
     dd = {(i+1): ev_distro[i] for i in range(10)}
     s = sum(dd.values())
     dd = {k: v/s for k, v in dd.items()}
